f560cb27fa08545c18f381120e5a6d796d47e628
LucasLiraDeOliveira <lucas.oliveira845@gmail.com>
2024-12-03 23:13:33 -0300

    adaptado o app.js para receber o menu lateral

--- atividade-pratica10/src/App.js
+++ atividade-pratica10/src/App.js
@@ -1,9 +1,18 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import SwipeableTemporaryDrawer from "./pages/Menu/SwipeableSideDrawer.jsx";
 
 function App() {
   return (
     <div className="App">
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <p><strong>Menu lateral vindo do Material UI </strong></p>
+        <strong><SwipeableTemporaryDrawer /></strong>
+      </div>
       <Header />
       <ListarTarefa />
     </div>
